fbd069cbbf108af47f8e29060875746288294365
genie-ru <genie.zgmf987@gmail.com>
2025-03-09 02:47:22 +0900

    test

---
+++ src/section2/matcher_func.test.ts
@@ -0,0 +1,12 @@
+it("数値のテスト", () => {
+  expect(2 + 2).toBe(4);
+}
+);
+it("文字列のテスト", () => {
+  expect("Hello, world!").toBe("Hello, world!");
+}
+);
+it("真偽値のテスト", () => {
+  expect(true).toBe(true);
+}
+);
